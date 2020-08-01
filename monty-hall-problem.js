bed4616259c87957cbc3575888d178e4b113dd31
Martin <office@martinpagels.de>
2020-08-02 01:25:15 +0200

    Initial commit

---
+++ monty-hall-problem.js
@@ -0,0 +1,43 @@
+const games = {
+  win: 0,
+  lost: 0,
+};
+
+const SWITCH_DOORS = true;
+const GAMES_TO_PLAY = 10000;
+
+// true = win(a car, a vacation) / false = lost(zonk!)
+const doorSetup = {
+  0: [true, false, false],
+  1: [false, true, false],
+  2: [false, false, true],
+};
+
+let counter = 0;
+while (counter < GAMES_TO_PLAY) {
+  playgame(SWITCH_DOORS);
+  counter++;
+}
+
+console.log("Wins:", games.win, "Lost:", games.lost);
+
+function playgame(switchDoors) {
+  // generate random three doors with one win two losses
+  let doorsToPlay = doorSetup[getRandomNumber(0, 3)];
+
+  // player chooses random a door
+  let playerChoose = doorsToPlay[getRandomNumber(0, 3)];
+
+  // gamemaster deletes one false door, now you decide, to switch door or not
+  let endOfGame = SWITCH_DOORS ? !playerChoose : playerChoose;
+
+  if (endOfGame) {
+    games.win += 1;
+  } else {
+    games.lost += 1;
+  }
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Monty Hall problem
+
+> Suppose you're on a game show, and you're given the choice of three doors: Behind one door is a car; behind the others, goats. You pick a door, say No. 1, and the host, who knows what's behind the doors, opens another door, say No. 3, which has a goat. He then says to you, "Do you want to pick door No. 2?" Is it to your advantage to switch your choice?
+
+A small side project of mine, using Javascript to prove that it would be better to change the door at the point where you are asked if you want to change the door.
